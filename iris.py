518829abbfff917bf86db077f230a6020b9878e9
ohmgmatt <bonilla.matthew44@gmail.com>
2018-08-22 09:26:34 -0700

    Split dataset for split testing

--- iris.py
+++ iris.py
@@ -2,8 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import GridSearchCV, train_test_split, KFold, \
+                                    cross_val_score, StratifiedKFold
+
+
+
+
+
 ## Import the dataset
-data = pd.read_csv('iris_working.csv', header =None,
+dataset = pd.read_csv('iris_working.csv', header =None,
                 names = ['sepal_length', 'sepal_width', 'petal_length','petal_width','class'])
-# print(data.head())
-# print(data.describe())
+# print(dataset.head())
+# print(dataset.describe())
+features = dataset.values[:,0:4]
+labels = dataset.values[:,4]
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.2, random_state=7)
